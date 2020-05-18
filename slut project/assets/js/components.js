77b216ae5e6b7bb52e6048d25a6d5ac42a089971
Tobias Hagselius <tobias.hagselius@hotmail.se>
2020-05-18 11:48:47 +0200

    working onclick cart btn and a broken imgslider script

--- slut project/assets/js/components.js
+++ slut project/assets/js/components.js
@@ -1,5 +1,21 @@
-/*on right click move img right 1000 px same on left  */
+function cart() {
+    var drop = document.getElementById("cartdropdown");
+    drop.classList.toggle("show");
+ }
 
-/*select/ find element */
-/*click on elements */
-/*result? */
+ var margins = 0;
+var imgContainer = document.querySelector(".image-slider");
+ var leftBtn = document.getElementsByClassName("left-button")[0];
+ var rightBtn = document.getElementsByClassName("right-button")[0];
+ var imgSlides = document.querySelector(".images");
+
+ leftBtn.addEventListener("click", function() {
+        margins = margins + 1000;
+         imgSlides.style.marginLeft += margins+"px";
+
+ })
+
+ rightBtn.addEventListener("click", function() {
+
+        imgSlides.style.marginLeft += -margins+"px";
+})
