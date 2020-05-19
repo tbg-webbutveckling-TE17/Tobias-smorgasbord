d0bd4f28441022ce9e27120e028c8eb385f72d5f
Tobias Hagselius <tobias.hagselius@hotmail.se>
2020-05-19 13:56:48 +0200

    imgslider left working not the right one

--- slut project/assets/js/components.js
+++ slut project/assets/js/components.js
@@ -8,14 +8,72 @@ var imgContainer = document.querySelector(".image-slider");
  var leftBtn = document.getElementsByClassName("left-button")[0];
  var rightBtn = document.getElementsByClassName("right-button")[0];
  var imgSlides = document.querySelector(".images");
+ 
+ var marginPx = 0;
 
  leftBtn.addEventListener("click", function() {
-        margins = margins + 1000;
-         imgSlides.style.marginLeft += margins+"px";
+        
+        if(marginPx == 0) {
+                marginPx = marginPx - 1000;
+                imgSlides.style.marginLeft = marginPx + "px";
+        }
+
+        else if (marginPx == -1000) {
+                marginPx = marginPx - 1000;
+                imgSlides.style.marginLeft = marginPx + "px";
+        }
+
+        else if (marginPx == -2000) {
+                marginPx = marginPx - 1000;
+                imgSlides.style.marginLeft = marginPx + "px";
+        }
+
+        else if (marginPx == -3000) {
+                marginPx = marginPx - 1000;
+                imgSlides.style.marginLeft = marginPx + "px";
+        }
+
+        else if (marginPx == -4000) {
+                marginPx = marginPx - 1000;
+                imgSlides.style.marginLeft = marginPx + "px";
+        } 
+        
+        else {
+                marginPx = 0;
+                imgSlides.style.marginLeft = marginPx + "px";
+        }
 
  })
 
  rightBtn.addEventListener("click", function() {
 
-        imgSlides.style.marginLeft += -margins+"px";
+        if(marginPx == 0) {
+                marginPx = marginPx + 1000;
+                imgSlides.style.marginRight = marginPx + "px";
+        }
+
+        else if (marginPx == 1000) {
+                marginPx = marginPx + 1000;
+                imgSlides.style.marginRight = marginPx + "px";
+        }
+
+        else if (marginPx == 2000) {
+                marginPx = marginPx + 1000;
+                imgSlides.style.marginRight = marginPx + "px";
+        }
+
+        else if (marginPx == 3000) {
+                marginPx = marginPx + 1000;
+                imgSlides.style.marginRight = marginPx + "px";
+        }
+
+        else if (marginPx == 4000) {
+                marginPx = marginPx + 1000;
+                imgSlides.style.marginRight = marginPx + "px";
+        } 
+        
+        else {
+                marginPx = 0;
+                imgSlides.style.marginRight = marginPx + "px";
+        }
 })
